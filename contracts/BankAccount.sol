0471716c226a4d576d4f697588b4334323eda9fa
tas-indranil <codearchitectdeveloper@gmail.com>
2024-01-22 05:37:41 +0530

    stubbed all the necessery functions

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -43,4 +43,88 @@ contract BankAccount {
         uint balance;   // total balance of the account
         mapping(uint => WithdrawRequest) withdrawRequests;  // the uint are going to be the ids of the WithdrawRequest
     }
+
+    //Associate account id and withdraw ids 
+    mapping(uint => Account) accounts;
+    mapping(address => uint[]) userAccount; // store ids of the accounts a user owned
+
+    uint nextAccountId;
+    uint nextWithdrawId;
+
+    //==========================================================================================================================
+    /*
+    User can have multiple accounts, so need to specify account id
+    */    
+    function deposit(uint accountId) external payable 
+    {
+
+    }
+
+
+    /*
+    Take in all of the owners, the person who calles this function will be automatically be the owner of the account
+    and this is external because this is not suppose to be called inside of the contract
+    */
+    function createAccount(address[] calldata otherOwners) external
+    {
+
+    }
+
+
+    function requestWithdrawl(uint accountId, uint ammount) external 
+    {
+
+    }
+
+
+    /*
+    We need to know which withdraw in the account we need to approve, because at a time multiple withdrawl request can exist
+    */
+    function approveWithdrawl(uint accountId, uint withdrawlId) external 
+    {
+
+    }
+
+
+    /*
+    This will give you money once the a withdrawl is approved
+    */
+    function withdraw(uint accountId, uint withdrawlId) external 
+    {
+
+    }
+
+
+    function getBalance(uint accountId) public view returns (uint)
+    {
+
+    }
+
+
+    /*
+    This will tell us what owners are associated with an account
+    */
+    function getOwners(uint accountId) public view returns (address[] memory)
+    {
+
+    }
+
+
+    /*
+    This will return number of approvals not whose approved
+    */
+    function getApprovals(uint accountId, uint withdrawlId) public view returns (uint)
+    {
+
+    }
+
+
+    /*
+    This will return ids of the account that this specific user is owner of
+    */
+    function getAccount() public view returns (uint[] memory) 
+    {
+        
+    }
+
 }
