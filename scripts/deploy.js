c28e2869681b88e8989ddda9dbe806087c34f040
tas-indranil <codearchitectdeveloper@gmail.com>
2024-02-05 22:57:20 +0530

    Created basic front end which is  connecting with metamask, creating account with different owners

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,5 @@
-// const hre = require("hardhat");
-// const fs = require("fs/promises");
-
-// async function main() {
-//   const bankAccount = await hre.ethers.deployContract("BankAccount");
-  
-
-//   await bankAccount.waitForDeployment();
-//   console.log('deployed to ${bankAccount.target}')
-//   await writeDeploymentInfo(bankAccount);
-// }
-
-// async function writeDeploymentInfo(contract) {
-//   const data = {
-//     contract: {
-//       address: contract.address,
-//       signerAddress: contract.signer.address,
-//       abi: contract.interface.format(),
-//     },
-//   };
-
-//   const content = JSON.stringify(data, null, 2);
-//   await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
 const hre = require("hardhat");
+const fs = require("fs/promises");
 
 async function main() { 
 
@@ -40,7 +10,19 @@ async function main() {
   console.log(
     `deployed to ${bankAccount.target}`
   );
+  await writeDeploymentInfo(bankAccount);
 }
+
+async function writeDeploymentInfo(contract) {
+    const data = {
+      contract: {
+        abi: contract.interface.format(),
+      },
+    };
+  
+    const content = JSON.stringify(data, null, 2);
+    await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
+  }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
